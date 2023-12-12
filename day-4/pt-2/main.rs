e3e5a04fcd95be221a25cadc95457500e907b661
Jamie R <jami3507@gmail.com>
2023-12-12 12:43:41 +0900

    Day 4

--- day-4/pt-2/main.rs
+++ day-4/pt-2/main.rs
@@ -3,9 +3,14 @@ use std::fs;
 fn main() {
     let contents: Vec<String> = fs::read_to_string("../input.txt").unwrap().lines().map(String::from).collect();
 
-    let mut sum = 0;
+    let mut card_count: Vec<u32> = vec![0; contents.len()];
 
-    for card in contents.iter() {
+    for _ in 1..0 {
+        println!("test");
+    }
+
+    for (i, card) in contents.iter().enumerate() {
+        card_count[i] += 1;
         let mut card_val = 0;
         let winning_nums: Vec<u32> = card.split(":").map(String::from).collect::<Vec<String>>()[1].split("|").map(String::from).collect::<Vec<String>>()[0].trim().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect();
 
@@ -13,16 +18,18 @@ fn main() {
 
         for num in obtained_nums.iter() {
             if winning_nums.contains(&num) {
-                if card_val == 0 {
-                    card_val = 1;
-                } else {
-                    card_val *= 2;
-                }
+                card_val += 1;
             }
         }
 
-        sum += card_val;
+        for _ in 0..card_count[i] {
+            for j in 1..card_val+1 {
+                card_count[i+j] += 1;
+            }
+        }
     }
+
+    let sum: u32 = card_count.iter().sum();
 
     println!("Sum: {}", sum);
 }
